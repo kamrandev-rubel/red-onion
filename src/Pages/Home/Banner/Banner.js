da7f9b608edd0288d406c8ba7d21eec9e7dc30e5
kamrandev-rubel <kamranuddin.rubel1@gmail.com>
2022-04-15 23:21:30 +0300

    feth data

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -9,7 +9,7 @@ const Banner = () => {
             <h1 className='text-6xl'>Best food waiting for your belly</h1>
             <div className='mt-[35px]'>
                 <input type="text" name="" id="" className='outline-none border-0 w-[500px] h-[55px] rounded-full px-5 text-lg' placeholder='Search food items' />
-                <button type="button" class="text-white bg-red-500 rounded-full h-[55px] w-32 relative left-[-80px]">Search</button>
+                <button type="button" className="text-white bg-red-500 rounded-full h-[55px] w-32 relative left-[-80px]">Search</button>
             </div>
         </div>
     );
--- src/Pages/Home/FoodFilter/FoodFilter.js
+++ src/Pages/Home/FoodFilter/FoodFilter.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const FoodFilter = () => {
+    const [allFoods, setAllFoods] = useState([]);
+    const [folterFoods, setFilterFoods] = useState([]);
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setAllFoods(data))
+    }, [])
+    const handleBreakfastFilter = () => {
+        const breakfastFood = allFoods.filter(food => food.category === 'breakfast')
+        setFilterFoods(breakfastFood);
+    }
+    const handleDinnerFilter = () => {
+        const dinnerFood = allFoods.filter(food => food.category === 'dinner')
+        setFilterFoods(dinnerFood);
+    }
+    const handleLunchFilter = () => {
+        const lunchFood = allFoods.filter(food => food.category === 'lunch')
+        setFilterFoods(lunchFood);
+    }
+    
     return (
         <div>
             <div className='flex justify-center items-center my-[95px]'>
-                <button className='mr-16 text-xl font-[550] hover:text-red-500 hover:border-b-4 hover:border-red-500' >Breakfast</button>
-                <button className='mr-16 text-xl font-[550] hover:text-red-500 hover:border-b-4 hover:border-red-500' >Lunch</button>
-                <button className='mr-16 text-xl font-[550] hover:text-red-500 hover:border-b-4 hover:border-red-500' >Dinner</button>
+                <button onClick={handleBreakfastFilter} className='mr-16 text-xl font-[550] hover:text-red-500 hover:border-b-4 hover:border-red-500' >Breakfast</button>
+                <button onClick={handleDinnerFilter} className='mr-16 text-xl font-[550] hover:text-red-500 hover:border-b-4 hover:border-red-500' >Lunch</button>
+                <button onClick={handleLunchFilter} className='mr-16 text-xl font-[550] hover:text-red-500 hover:border-b-4 hover:border-red-500' >Dinner</button>
             </div>
             <div>
-                <h2>food items</h2>
+                <h2>food items Lorem ipsum dolor sit amet.</h2>
+                {
+                    folterFoods.map(food => {
+                        const { id, picture, name, description } = food;
+                        return (
+                            <div key={food.id}>
+                                <img src={picture} alt="" className='w-10' />
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
     );
